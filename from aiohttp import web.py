167e62c4326d0a918a0d50ab6f1f76ffa6142886
Enroteg <Koryakin282@yandex.ru>
2023-01-24 15:32:56 +0500

    aiohttp

--- from aiohttp import web.py
+++ from aiohttp import web.py
@@ -1,8 +1,10 @@
 from aiohttp import web
 import vk_api
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import json
 import requests
-
+import time
+#https://discord.com/app/invite-with-guild-onboarding/nHuSrt3p6W
 token='e857793a4ecc55efad8627bd0873474822dbf6c111a31929518dd773678e53f57050dd879014fa3979f33'
 group_id1 = 194284961
 secret_key = '65193637EB7F4FC5335BD31D581FA'
@@ -11,7 +13,6 @@ url = 'http://transfer.commanda.keenetic.link/post'
 title = 'newTest'
 
 token='e857793a4ecc55efad8627bd0873474822dbf6c111a31929518dd773678e53f57050dd879014fa3979f33'
-
 vk_session = vk_api.vk_api.VkApiGroup(token=token, api_version='5.200')
 vk = vk_session.get_api()
 
@@ -21,21 +22,28 @@ confirmation_code = vk.groups.getCallbackConfirmationCode(access_token=token, gr
 
 routes = web.RouteTableDef()
 
-def send_message(peer_id,group_id,textFW,conversation_message_id):
+def send_message(peer_id,group_id,textFW,conversation_message_id, keyboard_message=None):
     
     fwmsg = {"peer_id":peer_id,"is_reply":1,"conversation_message_ids":conversation_message_id}
     fwmsgJson = json.dumps(fwmsg)
-    #fwmsgJson = web.json_response(fwmsg)
-    vk.messages.send(peer_id=peer_id, group_id=group_id, message=textFW,random_id=vk_api.utils.get_random_id(), forward=fwmsgJson)
+    
+    if keyboard_message == None:
+        vk.messages.send(peer_id=peer_id, group_id=group_id, message=textFW,random_id=vk_api.utils.get_random_id(), forward=fwmsgJson)
+    else: 
+        keyboard = VkKeyboard(one_time=False)
+        keyboard.add_callback_button(label='Кнопка 1', payload=['button_1'], color=VkKeyboardColor.POSITIVE)
+        keyboard.add_line()
+        keyboard.add_callback_button(label='Discord', payload=['open_discord_link'], color=VkKeyboardColor.PRIMARY)
+        vk.messages.send(peer_id=peer_id, group_id=group_id, message=textFW,random_id=vk_api.utils.get_random_id(), forward=fwmsgJson, keyboard=keyboard.get_keyboard())
 
 @routes.post('/post')
 async def hello(request):
+    
     data = await request.json()
     message_type = data['type']
 
     if message_type == 'confirmation':
-        return confirmation_code
-        #return ('6261e0d6')
+        return web.Response(text=confirmation_code)
     
     elif message_type == 'message_new':
         group_id = data['group_id']#int
@@ -54,12 +62,12 @@ async def hello(request):
         except Exception as err:
             print(err)
         
-        word_set = set(['aboba','glad','ранг'])    
+        word_set = set(['aboba','glad','ранг','клавиатура'])    
         phrase_set = set(text.split())
         #|||||||||||||||||||||||||||||
         if word_set.intersection(phrase_set) == set(['aboba']):
             
-            textFW = "abobus"+" "+str(date)
+            textFW = "abobus"
             send_message(peer_id,group_id,textFW,conversation_message_id)
             
         elif word_set.intersection(phrase_set) == set(['glad']):
@@ -73,13 +81,38 @@ async def hello(request):
             data = r.json()
             textFW = data['rank_tier']
             
-            send_message(peer_id,group_id,textFW,conversation_message_id)
+        elif word_set.intersection(phrase_set) == set(['клавиатура']):
+            textFW = "Клавиатура отправлена"
+            send_message(peer_id,group_id,textFW,conversation_message_id,keyboard_message=True)
+            
         return web.Response(text='ok')
-
-
+    
+    elif message_type == 'message_event':
+        button_group_id = data['group_id']#int
+        button_user_id = data['object']['user_id']
+        button_peer_id = data['object']['peer_id']#int
+        button_event_id = data['object']['event_id']#int
+        button_payload = data['object']['payload']
+        button_secret = data['secret']
+        
+        if button_payload[0] == "button_1":
+            snack = {"type": "show_snackbar","text": "Тут нихуя нет, долбоеб"}
+        elif button_payload[0] == "open_discord_link": 
+            snack = {"type": "open_link","link": "https://discord.gg/nHuSrt3p6W"}
+            
+        event_data = json.dumps(snack)
+        try:
+            vk.messages.sendMessageEventAnswer(event_id = button_event_id,
+                                                user_id = button_user_id,
+                                                peer_id = button_peer_id,
+                                                event_data = event_data)
+        except Exception as err:
+            print(err)
+                            
+        #return web.Response(text='ok')
+    return web.Response(text='ok')
 
 app = web.Application()
 
-
 app.add_routes(routes)
-web.run_app(app, host="0.0.0.0", port = "23659")
+web.run_app(app, host="0.0.0.0", port = 23659)
